a925b840431f4d9bfada47993608b72b41dd8cf9
Rodolpho Reis <rodolphoreis@live.com>
2024-05-17 20:46:52 +0100

    feat: create const UserProvider

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -5,3 +5,11 @@ const UserContext = createContext(undefined);
 type UserProviderProps = {
   children: React.ReactNode;
 };
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+  return (
+    <UserContext.Provider value={{ name: João }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
